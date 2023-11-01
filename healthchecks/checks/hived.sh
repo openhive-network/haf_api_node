d7ca303e3855701afe9029a0350eadb399f3d356
Eric Frias <efrias@syncad.com>
2023-11-01 15:13:17 +0000

    Update hived healthcheck to work with new node status api plugin

--- healthchecks/checks/hived.sh
+++ healthchecks/checks/hived.sh
@@ -2,18 +2,18 @@
 
 # Setup a trap to kill potentially pending wget at script exit
 trap "trap - SIGINT SIGTERM && kill -- -\$\$" SIGINT SIGTERM
-WGET_RESULT=$(wget -q -O - --post-data '{"jsonrpc": "2.0", "id": 1, "method": "condenser_api.get_dynamic_global_properties", "params": []}' http://haf:8091/)
+WGET_RESULT=$(wget -q -O - --post-data '{"jsonrpc": "2.0", "id": 1, "method": "node_status_api.get_node_status", "params": {}}' http://haf:8091/)
 if [ $? -ne 0 ]; then
   echo "down #wget returned an error"
   exit 1
 fi
 
 if echo "$WGET_RESULT" | grep -q '"error":'; then
-  echo "down #get_dgp() call returned an error"
+  echo "down #get_node_status() call returned an error"
   exit 2
 fi
 
-HIVED_HEAD_BLOCK_TIME_STRING=$(echo "$WGET_RESULT" | sed 's/^.*"time":"\([^"]*\)".*/\1/g')
+HIVED_HEAD_BLOCK_TIME_STRING=$(echo "$WGET_RESULT" | sed 's/^.*"last_processed_block_time":"\([^"]*\)".*/\1/g')
 HIVED_HEAD_BLOCK_TIME_EPOCH=$(date +%s -d $(echo $HIVED_HEAD_BLOCK_TIME_STRING | tr -- -T .-))
 CURRENT_TIME_EPOCH=$(date +%s)
 HEAD_BLOCK_AGE_SEC=$(expr $CURRENT_TIME_EPOCH - $HIVED_HEAD_BLOCK_TIME_EPOCH)
