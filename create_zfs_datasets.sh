31f1112b6b7b69b00256989d594cf255fdc376df
DanEric <dan@syncad.com>
2023-11-16 21:59:13 +0000

    Fix permissions on logs directory

--- create_zfs_datasets.sh
+++ create_zfs_datasets.sh
@@ -104,5 +104,7 @@ chown -R 105:109 "$TOP_LEVEL_DATASET_MOUNTPOINT/haf_postgresql_conf.d"
 
 mkdir -p "$TOP_LEVEL_DATASET_MOUNTPOINT/logs/postgresql"
 mkdir -p "$TOP_LEVEL_DATASET_MOUNTPOINT/logs/pgbadger"
+# 1000:100 is hived:users inside the container
+chown -R 1000:100 "$TOP_LEVEL_DATASET_MOUNTPOINT/logs"
 # 105:109 is postgres:postgres inside the container
 chown -R 105:109 "$TOP_LEVEL_DATASET_MOUNTPOINT/logs/postgresql" "$TOP_LEVEL_DATASET_MOUNTPOINT/logs/pgbadger"
