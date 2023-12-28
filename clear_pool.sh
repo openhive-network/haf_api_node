cc87459e7eb0528e3af9ee586facd1b8d33fe9da
Dan Notestein <dan@syncad.com>
2023-12-28 21:09:12 +0000

    add some simple scripts, mostly for me

---
+++ clear_pool.sh
@@ -0,0 +1,3 @@
+#brain dead script that needs improvement, but useful for me
+sudo rm -rf /haf-pool/haf-datadir/logs/postgresql/*
+sudo rm -rf /haf-pool/haf-datadir/haf_db_store/*
---
+++ make_ramdisk.sh
@@ -0,0 +1,2 @@
+#another braindead script for me
+sudo mount -t tmpfs -o size=26G ramdisk /mnt/ramfs
