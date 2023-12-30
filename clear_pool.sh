57afb4d051ada6d1588cf8514d394344053c19da
Dan Notestein <dan@syncad.com>
2023-12-30 05:08:38 +0000

    block-explorer-uninstall and clear haf_wal

--- clear_pool.sh
+++ clear_pool.sh
@@ -1,3 +1,5 @@
 #brain dead script that needs improvement, but useful for me
 sudo rm -rf /haf-pool/haf-datadir/logs/postgresql/*
 sudo rm -rf /haf-pool/haf-datadir/haf_db_store/*
+rm -rf /haf-pool/haf-datadir/blockchain/haf_wal/*
+rm /haf-pool/haf-datadir/bloockchain/shared_memory.bin
