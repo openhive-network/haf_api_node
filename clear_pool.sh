55844d4fcdbceedc87b7fe6b080292f341e519c8
SynaptiCAD User <sales@syncad.com>
2024-01-27 06:34:10 +0000

    allow clear_pool to work with arbitrary file locations, add hivemind role permission for hivemind installer

--- clear_pool.sh
+++ clear_pool.sh
@@ -1,6 +1,6 @@
 #brain dead script that needs improvement, but useful for me
-sudo rm -rf /haf-pool/haf-datadir/logs/postgresql/*
-sudo rm -rf /haf-pool/haf-datadir/haf_db_store/*
-rm -rf /haf-pool/haf-datadir/blockchain/haf_wal/*
-rm /haf-pool/haf-datadir/blockchain/shared_memory.bin
-rm /mnt/haf_shared_mem/shared_memory.bin
+. ./.env
+sudo rm -rf ${HAF_LOG_DIRECTORY}/postgresql/*
+sudo rm -rf ${HAF_DATA_DIRECTORY}/haf_db_store/*
+rm -rf ${HAF_DATA_DIRECTORY}/blockchain/haf_wal/*
+rm ${HAF_SHM_DIRECTORY}/shared_memory.bin
