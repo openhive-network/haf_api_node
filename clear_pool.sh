0a5e6f02a153aad6f63e6912e1bed9e77f51cbd4
Dan Notestein <dan@syncad.com>
2024-09-20 16:03:43 +0000

    Remove hard-coded revisions, and comment out as much as possible in the example environment file.

--- clear_pool.sh
+++ clear_pool.sh
@@ -1,5 +1,5 @@
 #brain dead script that needs improvement, but useful for me
 . ./.env
-sudo rm -rf ${HAF_LOG_DIRECTORY}/postgresql/*
-sudo rm -rf ${HAF_DATA_DIRECTORY}/haf_db_store/*
-rm -rf ${HAF_SHM_DIRECTORY}/shared_memory.bin ${HAF_SHM_DIRECTORY}/haf_wal/*
+sudo rm -rf ${HAF_LOG_DIRECTORY:-${TOP_LEVEL_DATASET_MOUNTPOINT}/logs}/postgresql/*
+sudo rm -rf ${HAF_DATA_DIRECTORY:-${TOP_LEVEL_DATASET_MOUNTPOINT}}/haf_db_store/*
+rm -rf ${HAF_SHM_DIRECTORY:-${TOP_LEVEL_DATASET_MOUNTPOINT}/shared_memory}/shared_memory.bin ${HAF_SHM_DIRECTORY:-${TOP_LEVEL_DATASET_MOUNTPOINT}/shared_memory}/haf_wal/*
