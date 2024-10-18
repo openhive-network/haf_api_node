55ac48486c72fc7a84503d64592e530134694b38
Konrad Botor <kbotor@syncad.com>
2024-10-18 14:57:23 +0200

    Update 2

--- ci/scripts/after-script.sh
+++ ci/scripts/after-script.sh
@@ -6,42 +6,19 @@ set -e
 if command -v docker &> /dev/null
 then
 
-  EXPECTED_SERVICES=(
-      caddy-1
-      haf-1
-      hafah-install-1
-      hafah-postgrest-1
-      hafah-postgrest-rewriter-1
-      haproxy-1
-      haproxy-healthchecks-1
-      hivemind-block-processing-1
-      hivemind-server-1
-      jussi-1
-      pgadmin-1
-      pghero-1
-      redis-1
-      semaphore-1
-      swagger-1
-      varnish-1
-      version-display-1
-  )
-
   cd /haf-api-node
 
   echo "Getting a list of services..."
-  docker compose ps --all > "${CI_PROJECT_DIR:?}/compose_processes.txt"
+  docker compose ps --all --format json > "${CI_PROJECT_DIR:?}/compose_processes.json"
 
   echo "Fetching stack logs..."
   docker compose logs --no-color > "${CI_PROJECT_DIR:?}/haf_api_node.log"
+ 
+  echo "Getting status of services..."
+  docker ps --all --format "{{.Names}}" | xargs -I {} bash -c "docker inspect --format \"{{json .State}}\" \"{}\" > \"{}.json\""
 
-  for SERVICE in "${EXPECTED_SERVICES[@]}"; do
-    echo "Getting status of $SERVICE..."
-    # Do not exit the script if the service is mising
-    docker inspect --format "{{json .State}}" "haf-world-${SERVICE}" > "${CI_PROJECT_DIR:?}/${SERVICE}-status.json" || true
-  done
-
+  echo "Shutting down the stack..."
   docker compose down --volumes
-
 fi
 
 cd "${CI_PROJECT_DIR:?}"
