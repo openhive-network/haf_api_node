babb158ea5674a6efacc46544d35cafcbc13402e
Dan Notestein <dan@syncad.com>
2024-11-15 23:10:23 +0000

    Make hivemind's healthcheck require reptracker to be in sync also

--- healthchecks/checks/hivemind.sh
+++ healthchecks/checks/hivemind.sh
@@ -8,6 +8,13 @@ trap "trap - 2 15 && kill -- -\$\$" 2 15
 
 check_haf_lib
 
+REPTRACKER_LAST_PROCESSED_BLOCK_AGE=$(psql "$POSTGRES_URL_REPTRACKER" --quiet --no-align --tuples-only --command="select extract('epoch' from hive.get_app_current_block_age('reptracker_app'))::integer")
+if [ "$REPTRACKER_LAST_PROCESSED_BLOCK_AGE" -gt 60 ]; then
+  age_string=$(format_seconds "$REPTRACKER_LAST_PROCESSED_BLOCK_AGE")
+  echo "down #reptracker_app block over a minute old ($age_string)"
+  exit 3
+fi
+
 HIVEMIND_LAST_IMPORTED_BLOCK_AGE=$(psql "$POSTGRES_URL" --quiet --no-align --tuples-only --command="select extract('epoch' from hive.get_app_current_block_age('hivemind_app'))::integer")
 if [ "$HIVEMIND_LAST_IMPORTED_BLOCK_AGE" -gt 60 ]; then
   age_string=$(format_seconds "$HIVEMIND_LAST_IMPORTED_BLOCK_AGE")
