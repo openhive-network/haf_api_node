08f410ecd8427c0a5f5bf6cff0b7def09ebed94c
Marcin Ickiewicz <mickiewicz@syncad.com>
2025-03-04 15:00:02 +0100

    add hivesense to the api node

---
+++ healthchecks/checks/hivesense.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+set -e
+
+. "$(dirname "$0")/check_haf_lib.sh"
+
+# Setup a trap to kill potentially pending healthcheck SQL query at script exit
+trap "trap - 2 15 && kill -- -\$\$" 2 15
+
+check_haf_lib
+
+HIVESENSE_LAST_PROCESSED_BLOCK_AGE=$(psql "$POSTGRES_URL_HIVESENSE" --quiet --no-align --tuples-only --command="select extract('epoch' from hive.get_app_current_block_age('hivesense_app1'))::integer")
+if [ "$HIVESENSE_LAST_PROCESSED_BLOCK_AGE" -gt 60 ]; then
+  age_string=$(format_seconds "$HIVESENSE_LAST_PROCESSED_BLOCK_AGE")
+  echo "down #hivesense_app block over a minute old ($age_string)"
+  exit 3
+fi
+
+echo "up"
+exit 0
