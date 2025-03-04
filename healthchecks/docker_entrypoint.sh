7dc151cf3a567765a830361dfafd007a140ba2f5
Marcin Ickiewicz <mickiewicz@syncad.com>
2025-03-04 15:00:02 +0100

    add hivesense to the api node

--- healthchecks/docker_entrypoint.sh
+++ healthchecks/docker_entrypoint.sh
@@ -6,5 +6,6 @@ nc -lk -p 7003 -e /checks/hafah.sh &
 nc -lk -p 7004 -e /checks/hafbe_btracker.sh &
 nc -lk -p 7005 -e /checks/hafbe.sh &
 nc -lk -p 7009 -e /checks/hafbe_reptracker.sh &
+nc -lk -p 7011 -e /checks/hivesense.sh &
 
 wait
